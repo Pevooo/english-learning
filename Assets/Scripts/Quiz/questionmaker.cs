16db6eae7d2b1a361a3dd5f2f7181e7ada9198ba
Marley Amged <mollyamged89@gmail.com>
2024-12-17 17:41:18 +0200

    quiz

--- Assets/Scripts/Quiz/questionmaker.cs
+++ Assets/Scripts/Quiz/questionmaker.cs
@@ -4,36 +4,9 @@ using System.Numerics;
 using UnityEngine;
 using System;
 public class QuestionMaker : MonoBehaviour
-{
-    public Sprite questionSprite;
-    public Sprite choice1;
-    public Sprite choice2;
-    public Sprite choice3;
+{    
     public GameObject rightAnswer;
-    public GameObject[] GetChildren(GameObject parent)
-    {
-        int childCount = parent.transform.childCount;
-        GameObject[] children = new GameObject[childCount];
-
-        for (int i = 0; i < childCount; i++)
-        {
-            children[i] = parent.transform.GetChild(i).gameObject;
-        }
-
-        return children;
-    }
-
-
-    void Start()
-    {
-        GameObject[] childObjects = GetChildren(gameObject);
-
-        // childObjects[0].GetComponent<SpriteRenderer>().sprite = choice1;
-        // childObjects[1].GetComponent<SpriteRenderer>().sprite = choice2;
-        // childObjects[2].GetComponent<SpriteRenderer>().sprite = choice3;
-        // childObjects[3].GetComponent<SpriteRenderer>().sprite = questionSprite;
-    }
-
+    
     public void DestroyQuestion() {
         Destroy(gameObject);
     }
