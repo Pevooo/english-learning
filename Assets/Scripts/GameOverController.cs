01ad82bb9ac35d7778cdafe510ee6cb96264d6a6
Pavly <pavlysamuelatef@gmail.com>
2024-12-19 19:08:33 +0200

    Add Effects

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameOverController : MonoBehaviour
 {
+    void Start() {
+        Destroy(GameObject.Find("Videoplayer"), 4.6f);
+        Destroy(GameObject.Find("Screen"), 4.6f);
+    }
     public void TryAgain() {
         SceneManager.LoadScene("Quiz");
     }
